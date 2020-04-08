a72590fdd678065938e9ee187beb16cb811d36e8
WeiCSir <49602537+WeiCSir@users.noreply.github.com>
2020-04-08 19:10:46 +0800

    final_insertSort

--- insertSort/main.cpp
+++ insertSort/main.cpp
@@ -4,20 +4,16 @@ using namespace std;
 int main()
 {
 	int num[10] = { 3,5,7,7,5,8,934,65,78,56};
-
+	int j;
 	for (int i = 1; i < 10; ++i)
 	{
-		int j = i;
-		while (num[j] < num[j - 1])
+		int sortNum = num[i];
+		for (j = i-1; sortNum > num[j] && j >=0; j--)
 		{
-			int temp = num[j];
-			num[j] = num[j - 1];
-			num[j - 1] = temp;
-			j--;
-			if (j == 0)
-				break;
-
+			num[j+1]= num[j];
 		}
+
+		num[j+1]=sortNum;
 	}
 
 
